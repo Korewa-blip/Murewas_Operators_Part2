7c9a0fc39b9ee7f866cb7da7f2a0fbb32ccb731f
Korewa-blip <murekosh@gmail.com>
2025-08-03 19:39:13 +0100

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,8 @@
+print("Enter a Number (Numerator)")
+num=int(input())
+print("Enter a Number(Denominator)")
+num1=int(input())
+if num%num1==0:
+ print("Numerator is divisible by Denominator")
+else:
+ print("Numerator is not divisible by Denominator")
